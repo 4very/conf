e85795bc495796b82dcc59433311d97f4b3c3d8b
Avery Sommer <asommer999@gmail.com>
2022-04-11 22:09:17 -0400

    uncommented some lines

--- scripts/cont_install.sh
+++ scripts/cont_install.sh
@@ -1,8 +1,8 @@
 git clone https://github.com/4very/conf.git /home/vscode/conf
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/vscode/.oh-my-zsh/custom/themes/powerlevel10k
 
-# mkdir /home/vscode/.antigen
-# curl -L git.io/antigen > /home/vscode/.antigen/antigen.zsh
+mkdir /home/vscode/.antigen
+curl -L git.io/antigen > /home/vscode/.antigen/antigen.zsh
 
 # rm /home/vscode/.zshrc
 # cp -a /home/vscode/conf/zsh/. /home/vscode/
